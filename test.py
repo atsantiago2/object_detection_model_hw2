0b3c82c33cf7f49cb033cf99999bc43c97254997
Alessandro <alessandro.santiago@eee.upd.edu.ph>
2022-05-01 17:22:52 +0800

    Added dataset downloader to 2 test files

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 #Download Dataset
+
+from src import downloader
+downloader.get_drinks_dataset()
+
 #Download Pre Trained Model
 #Show Performance Scores
--- train.py
+++ train.py
@@ -1,3 +1,7 @@
 #Download Dataset
+
+from src import downloader
+downloader.get_drinks_dataset()
+
 #Train using GPU
 #Show Performance Metrics
